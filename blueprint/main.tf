73d961e79c1ebb9276a090b4175a56f952ae5070
anushkasathe <anushka.sathe@genesys.com>
2022-10-11 11:43:22 -0400

    added client id and secet key for provider authentication

--- blueprint/main.tf
+++ blueprint/main.tf
@@ -17,6 +17,8 @@ terraform {
 
 provider "genesyscloud" {
   sdk_debug = true
+  oauthclient_id     = "339f87f2-1ad0-4bc0-a6b7-96f467852143"
+  oauthclient_secret = "Sv7h7CKlI6Yb13qiVh1rV11MOvZ1zULTPniedCw3Vqs"
   aws_region = "us-east-1"
 
 }
