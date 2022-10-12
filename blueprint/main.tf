cdbd7c83b995b919b97da8aa9ebb7f240790aaab
anushkasathe <113941423+anushkasathe@users.noreply.github.com>
2022-10-12 10:25:46 -0400

    Update main.tf

--- blueprint/main.tf
+++ blueprint/main.tf
@@ -17,8 +17,6 @@ terraform {
 
 provider "genesyscloud" {
   sdk_debug = true
-  oauthclient_id     = "339f87f2-1ad0-4bc0-a6b7-96f467852143"
-  oauthclient_secret = "Sv7h7CKlI6Yb13qiVh1rV11MOvZ1zULTPniedCw3Vqs"
   aws_region = "us-east-1"
 
 }
