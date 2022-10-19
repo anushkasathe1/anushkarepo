ac9cecbc9df15307937734998795f8f2b5888a7c
anushkasathe <anushka.sathe@genesys.com>
2022-10-19 15:52:58 -0400

    removing flow

--- genesys-cloud-architect-flows/main.tf
+++ genesys-cloud-architect-flows/main.tf
@@ -27,14 +27,5 @@ resource "genesyscloud_flow" "flow" {
 filepath = "https://bitbucket.org/anushkasathe/automatedflows/raw/707faa02f826731708f1498f3196c53af1f5e6d1/Archy%20Hello%20World_v1-0.yaml"
 }
 
-resource "genesyscloud_flow" "flow1" {  
-filepath = "https://bitbucket.org/anushkasathe/automatedflows/raw/707faa02f826731708f1498f3196c53af1f5e6d1/Archy%20Hello%20World_v1-0.yaml"
-}
-resource "genesyscloud_flow" "flow2" {  
-filepath = "https://bitbucket.org/anushkasathe/automatedflows/raw/707faa02f826731708f1498f3196c53af1f5e6d1/Archy%20Hello%20World_v1-0.yaml"
-}
-resource "genesyscloud_flow" "flow3" {  
-filepath = "https://bitbucket.org/anushkasathe/automatedflows/raw/707faa02f826731708f1498f3196c53af1f5e6d1/Archy%20Hello%20World_v1-0.yaml"
-}
 
        
